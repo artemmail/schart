c9f09ba8302eb0e9de899c729ec07c5d95604ac0
ruticker1 <ruticker@gmail.com>
2025-12-24 14:17:29 +0300

    remove

--- Angular/my-app1/src/app/models/MyMouseEvent.ts
+++
@@ -1,6 +0,0 @@
-import { Point } from './Point';
-
-export interface MyMouseEvent {
-    position: Point;
-    button: number;
-}
--- Angular/src/public-api.ts
+++
@@ -1,3 +0,0 @@
-// projects/color-picker/src/public-api.ts
-export * from './lib/color-picker.component';
-export * from './lib/color-picker.module';
