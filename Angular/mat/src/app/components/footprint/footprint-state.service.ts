f25efa424a886edaa9692f2cca4955c7a2eb3495
artemmail <artemmail@gmail.com>
2025-12-26 19:57:43 +0300

    Refactor footprint state handling

---
+++ Angular/mat/src/app/components/footprint/footprint-state.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { ChartSettings } from 'src/app/models/ChartSettings';
+import { FootPrintParameters } from 'src/app/models/Params';
+import { ChartSettingsService } from 'src/app/service/chart-settings.service';
+import { ClusterData } from './clusterData';
+
+export interface FootprintComponentState {
+  data: ClusterData | null;
+  params: FootPrintParameters | null;
+  settings: ChartSettings;
+  hiddenHint: boolean;
+  selectedPrice: number | null;
+  selectedPrice1: number | null;
+  dragMode: number | null;
+  viewInitialized: boolean;
+}
+
+@Injectable()
+export class FootprintStateService {
+  private state: FootprintComponentState = {
+    data: null,
+    params: null,
+    settings: ChartSettingsService.DefaultSettings(),
+    hiddenHint: true,
+    selectedPrice: null,
+    selectedPrice1: null,
+    dragMode: null,
+    viewInitialized: false,
+  };
+
+  get snapshot(): FootprintComponentState {
+    return this.state;
+  }
+
+  update(partial: Partial<FootprintComponentState>): void {
+    this.state = { ...this.state, ...partial };
+  }
+
+  clearSelection(): void {
+    this.update({ selectedPrice: null, selectedPrice1: null });
+  }
+
+  setHintHidden(hidden: boolean): void {
+    this.update({ hiddenHint: hidden });
+  }
+
+  markViewInitialized(): void {
+    this.update({ viewInitialized: true });
+  }
+}
