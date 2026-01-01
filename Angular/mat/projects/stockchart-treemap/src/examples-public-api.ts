2ddb56398ada2aab84e627f116b9d54a539bacd0
artemmail <artemmail@gmail.com>
2026-01-01 15:26:26 +0300

    Add build target for stockchart treemap examples

---
+++ Angular/mat/projects/stockchart-treemap/src/examples-public-api.ts
@@ -0,0 +1,3 @@
+export * from './lib/examples/app-stockchart-treemap-example.component';
+export * from './lib/examples/self-colored-treemap-example.component';
+export * from './lib/examples/mock-data';
