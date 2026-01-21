a87e77ad6717d954c829987740c0a0d0673ebcca
ruticker1 <ruticker@gmail.com>
2026-01-21 15:27:38 +0300

    диалог цвета

--- Angular/mat/src/app/components/Dialogs/level-settings-dialog/level-settings-dialog.component.ts
+++ Angular/mat/src/app/components/Dialogs/level-settings-dialog/level-settings-dialog.component.ts
@@ -5,7 +5,6 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { PalettePickerModule } from 'src/lib/palette-picker.module';
 import { MarkLineLevel } from 'src/app/service/FootPrint/LevelMarks/level-marks.service';
 
 
@@ -20,7 +19,6 @@ import { MarkLineLevel } from 'src/app/service/FootPrint/LevelMarks/level-marks.
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    PalettePickerModule,
   ],
 })
 export class LevelSettingsDialogComponent {
